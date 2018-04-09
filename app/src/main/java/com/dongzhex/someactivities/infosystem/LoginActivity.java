853a6bc979d3699dd9a2b9cbe9cf50cdf9e48164
dongzhe <ncxxdz@126.com>
2018-04-09 11:53:20 +0800

    second commit

--- app/src/main/java/com/dongzhex/someactivities/infosystem/LoginActivity.java
+++ app/src/main/java/com/dongzhex/someactivities/infosystem/LoginActivity.java
@@ -1,13 +1,59 @@
 package com.dongzhex.someactivities.infosystem;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class LoginActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_activity);
+        //设置标题栏
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //标题栏的home按钮
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar!=null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.system_zzz_keyboard_backspace);
+        }
+    }
+    //菜单初始化
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.login_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    //菜单点击时间
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.login_help_item:
+                AlertDialog.Builder dialog = new AlertDialog.Builder(LoginActivity.this);
+                dialog.setTitle("登录帮助:");
+                dialog.setMessage("本软件不提供登陆功能\n，请从管理员哪里获取账号密码进行登录");
+                dialog.setCancelable(true);
+                dialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+                break;
+            case android.R.id.home:
+                LoginActivity.this.finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/dongzhex/someactivities/infosystem/perfect_information_user.java
@@ -0,0 +1,13 @@
+package com.dongzhex.someactivities.infosystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class perfect_information_user extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfect_information_user);
+    }
+}
